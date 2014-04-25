d48473cbbf43f31f99d88cbbf5854f55556f8b6b
Duncan Burggraf <dcburggraf@gmail.com>
2014-04-25 11:41:31 -0700

    added prototypes for future features

--- include/Tree.h
+++ include/Tree.h
@@ -16,9 +16,15 @@ enum TreeGender { root, left, right };
 // ############### Method Prototypes ###############
 
 Tree * tree_init();
-int tree_destroy(Tree * tree);
-int tree_splay(Tree * node);
+int tree_destroy(Tree * tree); //frees this node and both subtrees
+int tree_splay(Tree * node); //if success, node will be the tree root
 int tree_splice(Tree * trunk, Tree * branch, enum TreeGender side);
+
+Tree * tree_getByIndex(Tree * root, int index);
+Tree * tree_insertByIndex(Tree * root, Tree * node, int index);
+Tree * tree_insertByKey(Tree * root, Tree * node);
+Tree * tree_balance(Tree * tree); //returns new subtree root, null on fail
+
 
 
 
@@ -28,20 +34,20 @@ struct Tree
 {
 	
 	Tree * leftTree;
-	int sizeLeft;
-	int depthLeft;
+	int sizeLeft; //number of nodes in left subtree
+	int depthLeft; //maximum depth of left subtree
 	
 	Tree * rightTree;
-	int sizeRight;
-	int depthRight;
+	int sizeRight; //number of nodes in right subtree
+	int depthRight; //maximum depth of left subtree
 	
-	int size;
-	int depth;
+	int size; //number of nodes in this tree
+	int depth; //maximum depth of this tree
 	
-	Tree * parent;
-	enum TreeGender gender;
+	Tree * parent; //points to parent node, if any
+	enum TreeGender gender; //flags node as a root or a left or right subtree
 	
-	int key;
+	int key; //if nodes are sorted by key, this is such a key
 	
 };
 
